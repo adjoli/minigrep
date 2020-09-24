85a4c1c3bc1958913c439cf7e46b6d4c0a1ed308
Adao Oliveira <adaum79@gmail.com>
2020-09-24 15:18:56 -0300

    Commit inicial

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problema ao processar argumentos: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Erro de aplicação: {}", e);
+        process::exit(1);
+    }
+}
+
+
+
+
